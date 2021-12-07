618b52036cfe8d8f22d14241d4b648924659ff16
Severin Goddon <severin.goddon@students.ffhs.ch>
2021-12-07 11:25:41 +0100

    optimize runtime: implement median

--- day7/Day7.py
+++ day7/Day7.py
@@ -1,20 +1,23 @@
-def findOptimumPos(part2):
+import statistics
+
+
+def findOptimumPos(part2, crabs):
     minimumFuelConsumption = sum(crabs) * 1000
-    for pos in range(max(crabs)):
+    for pos in range(int(statistics.median(crabsArray)), max(crabs)):
         consumption = 0
         for crab in crabs:
             difference = crab - pos
             if difference < 0: difference *= -1
             if part2: consumption += sum(range(difference + 1))
             else: consumption += difference
-        if consumption < minimumFuelConsumption: minimumFuelConsumption = consumption
+        if consumption < minimumFuelConsumption:minimumFuelConsumption = consumption
+        else: break
     print("Minimum consumption is: ", minimumFuelConsumption)
 
 
-crabs = []
-
 with open('input.txt') as file:
+    crabsArray = []
     for file_line in file:
-        for x in file_line.split(","): crabs.append(int(x))
-findOptimumPos(part2=False)
-findOptimumPos(part2=True)
+        for x in file_line.split(","): crabsArray.append(int(x))
+    findOptimumPos(part2=False, crabs=crabsArray)
+    findOptimumPos(part2=True, crabs=crabsArray)
