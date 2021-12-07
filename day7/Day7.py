f21e506c8a5f39aac4dd0c690fcc885409a9cc33
Severin Goddon <severin.goddon@students.ffhs.ch>
2021-12-07 13:10:25 +0100

    -optimized code

--- day7/Day7.py
+++ day7/Day7.py
@@ -2,17 +2,15 @@ import statistics
 
 
 def findOptimumPos(part2, crabs):
-    minimumFuelConsumption = sum(crabs) * 1000
-    for pos in range(int(statistics.median(crabsArray)), max(crabs)):
-        consumption = 0
-        for crab in crabs:
-            difference = crab - pos
-            if difference < 0: difference *= -1
-            if part2: consumption += sum(range(difference + 1))
-            else: consumption += difference
-        if consumption < minimumFuelConsumption:minimumFuelConsumption = consumption
-        else: break
-    print("Minimum consumption is: ", minimumFuelConsumption)
+    consumption = 0
+    mean = int(statistics.mean(crabs))
+    median = int(statistics.median(crabs))
+    for crab in crabs:
+        if part2: difference = crab - mean
+        else: difference = crab - median
+        if part2: consumption += sum(range(abs(difference) + 1))
+        else: consumption += abs(difference)
+    print(consumption)
 
 
 with open('input.txt') as file:
