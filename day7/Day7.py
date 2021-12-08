3a48d31e0b127f781496ee22b78859130d578926
Severin Goddon <severin.goddon@students.ffhs.ch>
2021-12-08 09:30:23 +0100

    changes

--- day7/Day7.py
+++ day7/Day7.py
@@ -1,14 +1,17 @@
 def findOptimumPos(part2):
+    totalIterations = 0
     minimumFuelConsumption = sum(crabs) * 1000
     for pos in range(max(crabs)):
         consumption = 0
         for crab in crabs:
+            totalIterations+=1
             difference = crab - pos
             if difference < 0: difference *= -1
             if part2: consumption += sum(range(difference + 1))
             else: consumption += difference
         if consumption < minimumFuelConsumption: minimumFuelConsumption = consumption
     print("Minimum consumption is: ", minimumFuelConsumption)
+    print("Iterations: ",totalIterations)
 
 
 crabs = []
@@ -16,5 +19,6 @@ crabs = []
 with open('input.txt') as file:
     for file_line in file:
         for x in file_line.split(","): crabs.append(int(x))
+print(len(crabs))
 findOptimumPos(part2=False)
 findOptimumPos(part2=True)
