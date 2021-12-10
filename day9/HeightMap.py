5ab132c95cb465e93f5cc4d1df65edc1ceeb94e9
Severin Goddon <severin.goddon@students.ffhs.ch>
2021-12-10 12:51:01 +0100

    show biggest three basins in the map

--- day9/HeightMap.py
+++ day9/HeightMap.py
@@ -1,8 +1,7 @@
 import tkinter as tk
-
 from day9.Day9Part2 import doWork
 
-
+#  this class draws a gui containing the height map. It draws all the steps of the recursive algorithm while it's finding all basins. At the end, it marks the three biggest basins in the map
 class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
@@ -10,9 +9,11 @@ class App(tk.Tk):
         self.canvas.pack(side="top", fill="both", expand="true")
         self.rows = 100
         self.columns = 100
-        self.cellwidth = 8
-        self.cellheight = 8
-        self.b = doWork()
+        self.cellwidth = 10
+        self.cellheight = 10
+        self.result = doWork()
+        self.b = self.result[0]
+        self.biggestBasin = self.result[1]
         self.count = 0
         self.rect = {}
         for column in range(100):
@@ -23,7 +24,7 @@ class App(tk.Tk):
                 y2 = y1 + self.cellheight
                 self.rect[row,column] = self.canvas.create_rectangle(x1,y1,x2,y2, fill="blue", tags="rect")
 
-        self.redraw(5)
+        self.redraw(2)
 
     def redraw(self, delay):
         row = self.b[self.count][0]
@@ -31,7 +32,19 @@ class App(tk.Tk):
         item_id = self.rect[row, col]
         self.canvas.itemconfig(item_id, fill="green")
         self.count+=1
-        self.after(delay, lambda: self.redraw(delay))
+        print(self.count)
+        if self.count< 8500:self.after(delay, lambda: self.redraw(delay))
+        else:
+            self.count = 0
+            self.after(delay, lambda: self.drawBiggestBasin(delay))
+
+    def drawBiggestBasin(self, delay):
+        row = self.biggestBasin[self.count][0]
+        col = self.biggestBasin[self.count][1]
+        item_id = self.rect[row, col]
+        self.canvas.itemconfig(item_id, fill="red")
+        self.count+=1
+        self.after(delay, lambda: self.drawBiggestBasin(delay))
 
 if __name__ == "__main__":
     app = App()
