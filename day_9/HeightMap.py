b44c6275830109972ae5c3f81ded09da5bc1e1c4
Severin Goddon <severin.goddon@students.ffhs.ch>
2021-12-11 10:57:01 +0100

    renaming directories

--- day_9/HeightMap.py
+++ day_9/HeightMap.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from day9.Day9Part2 import doWork
+from day_9.Day9Part2 import doWork
 
 #  this class draws a gui containing the height map. It draws all the steps of the recursive algorithm while it's finding all basins. At the end, it marks the three biggest basins in the map
 class App(tk.Tk):
